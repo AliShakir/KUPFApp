2589774becefdc4c7e0dd01b649957763c05130f
Johar Saifuddin Mandav Laptop <joharmandav@hotmail.com>
2022-07-13 22:30:55 +0300

    cmd file created

---
+++ runmyproject.cmd
@@ -0,0 +1,23 @@
+D:\GitHub\ShakKUPFApp this is the folder 
+can ustart the the VS Code and show me step by step and if u can make batch file for this is good
+sure see the stups
+this will open your project in VS code
+
+cmd code .
+next
+dotnet run
+dotnet publish -c Release -o ./publish
+ng update @angular/core --force
+
+some problem in the version or somethign
+
+Yes ye khud se update ho jata hai
+
+fir kya kare abhi
+?
+wiat for it  agar ho jata hai to thek hai warna ap ko src files day do ga par server pe dal k test karlena
+
+ok iske baad built publish kar dena
+api ki bat kar rahay hain?
+yes after this we can built publish or no 
+we are calling our live api and there no change in it so will wjust put angular project live and thiw will work.
